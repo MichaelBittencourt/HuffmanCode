bcf790d12c0d419eaa718b1e5502c63696e94bb0
MichaelBittencourt <mchl.bittencourt@gmail.com>
2016-05-19 00:25:40 -0300

    Atualizando informacoes da Interface de Dados

--- DadosCompressorIF.h
+++ DadosCompressorIF.h
@@ -4,13 +4,24 @@
 class DadosCompressorIF{
 	public:
 		DadosCompressorIF(){
+		// empty
 		}
 		virtual unsigned int getCountByte(char byte) = 0; //Metodos Referente a leitura e escrita dos dados.
 		/* Os metodos e estruturas aqui listadas devem tratar leitura e escritas dos dados, fornecendo
 		o conjunto de bytes necessarios para implementação do compressor. */
+		virtual unsigned char * getArrayFrequency() = 0;
+		virtual unsigned int getDados(unsigned char * buffer, const unsigned int size) = 0; //Retorna a quatidade real de dados lidos em caso de chegar ao fim dos dados
+		virtual unsigned char getPadding() = 0; //é um valor entre 0 e 7
+		virtual void write(const unsigned char * ArrayDados, const unsigned int size) = 0;
+		virtual void setTypeAction(const bool typeAction) = 0;
 		virtual void operator = (DadosCompressorIF & copia) = 0;//colocar const eu acho. Não sei se devo colocar aqui
-		~DadosCompressorIF(){
+		virtual ~DadosCompressorIF(){
+		// empty
 		}
+		
+		//Constantes destinadas ao uso com setTypeAction
+		const bool COMPRESS = true;
+		const bool DECOMPRESS = false;
 };
 
 #endif
